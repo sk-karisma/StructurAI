955597319ec6db18abe933527d38f2fbf85d67ed
Karisma <karisma@Karismas-MacBook-Pro.local>
2026-02-28 17:30:42 +0530

    Deployment fix

--- backend/services/gemini_client.py
+++ backend/services/gemini_client.py
@@ -13,14 +13,14 @@ client = genai.Client(api_key=GEMINI_API_KEY)
 
 def generate_json(prompt: str):
     response = client.models.generate_content(
-        model="gemini-1.5-flash",
+        model="gemini-2.5-flash",
         contents=prompt,
     )
     return response.text
 
 def generate_text(prompt: str):
     response = client.models.generate_content(
-        model="gemini-1.5-flash",
+        model="gemini-2.5-flash",
         contents=prompt,
     )
     return response.text
